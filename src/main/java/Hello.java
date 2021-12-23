4fc22875ac1145618bb0f00c9819758f8c08e422
nurcanuk <yusuferdoganuk@gmail.com>
2021-12-23 22:54:59 +0000

    Started Hello Feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Gitup Repo is already published to Github");
+        System.out.println("doing more work");
     }
 }
